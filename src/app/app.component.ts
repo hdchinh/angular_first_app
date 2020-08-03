4b6af3549acb3a82883f1ac205463b935efab333
Duy Chinh <hduychinh@gmail.com>
2020-08-03 16:29:44 +0700

    Small Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'The First Angular Web';
+  title = 'Angular Tutorial App';
 }
